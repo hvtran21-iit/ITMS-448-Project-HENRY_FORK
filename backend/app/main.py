373689c7c028f46e95a0300ca7210d1fa22d9ea9
Henry T <htran21@hawk.iit.edu>
2025-11-17 16:45:34 -0600

    added input sanitization

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,3 +18,14 @@ load_dotenv()
 client_id = os.getenv("CLIENT_ID")
 client_secret = os.getenv("CLIENT_SECRET")
 
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+# Load spaCy model
+nlp = spacy.load("en_core_web_sm")
+
+def word_tokenize(text):
+    """Tokenizes text using SpaCy, removes stop words and punctuation."""
+    doc = nlp(text)
+    tokens = [token.lemma_.lower() for token in doc if not token.is_stop and not token.is_punct]
+    return tokens
+
